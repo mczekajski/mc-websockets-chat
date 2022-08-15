9ff18030fc9b8fc48a3f77e783e3cec0d2296a0f
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-08-15 12:58:20 +0200

    remove express

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const WebSocket = require("ws");
-const express = require("express");
 
-const app = express();
 const port = process.env.PORT || 80;
 const wss = new WebSocket.Server({ port });
 
-app.get("/", (req, res) => {
-  res.send(`WebSocketServer is working on port ${port}`);
-});
-
 wss.on("connection", (client) => {
-  console.log(`WebSocketServer is working on port ${port}`);
   client.on("message", (message, isBinary) => {
     [...wss.clients]
       .filter((c) => c !== client)
