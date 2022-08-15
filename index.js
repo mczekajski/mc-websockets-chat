3bf2b92e52cf2b67b86cac51ffbe6f4dffe568ed
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-08-15 12:15:51 +0200

    procfile

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const PORT = process.env.PORT || 8080;
+
+const wss = new WebSocketServer({ PORT });
 
 wss.on("connection", (client) => {
   client.on("message", (message, isBinary) => {
