0ed1cff46fda4be0be38a9ac90e2bf7c53281c8c
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-08-15 13:37:38 +0200

    connection opened message

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const WebSocket = require("ws");
 const port = process.env.PORT || 80;
 const wss = new WebSocket.Server({ port });
 
-wss.onopen = () => {
-  wss.send(
-    JSON.stringify({
-      message: "Welcome to the chat. Type below to begin a conversation...",
-    })
-  );
-};
-
 wss.on("connection", (client) => {
   client.on("message", (message, isBinary) => {
     [...wss.clients]
