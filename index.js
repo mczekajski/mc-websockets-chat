d169006327e8ff514d4267692c05dc61e8b05c2a
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-08-15 12:40:27 +0200

    test

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const WebSocket = require("ws");
 const express = require("express");
 
 const app = express();
-const PORT = 8080;
-const wss = new WebSocket.Server({ port: PORT });
+const port = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port });
 
 app.get("/", (req, res) => {
-  res.send(`WebSocketServer is working on port ${PORT}`);
+  res.send(`WebSocketServer is working on port ${port}`);
 });
 
 wss.on("connection", (client) => {
+  console.log(`WebSocketServer is working on port ${port}`);
   client.on("message", (message, isBinary) => {
     [...wss.clients]
       .filter((c) => c !== client)
